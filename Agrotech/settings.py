5ecccc4e6787a544d45bf926453ad8d6a871786a
Md Sadique Amin <230223131018@gecpatan.ac.in>
2026-08-22 15:22:33 +0530

    fix: add .onrender.com to ALLOWED_HOSTS

--- Agrotech/settings.py
+++ Agrotech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dummy-key-for-build')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com', cast=Csv())
 
 # Enable Authentication Backend
 AUTHENTICATION_BACKENDS = [
